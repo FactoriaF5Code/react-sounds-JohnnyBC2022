669e12bf50a608beebb1a784c8c74c36eca0fdca
Johnny <jonathanbc2012@gmail.com>
2023-12-31 13:55:58 +0100

    components sounds and constant created

---
+++ src/components/guitar-contain/Sounds.jsx
@@ -0,0 +1,16 @@
+import eSound from "../../assets/sounds/e.mp3";
+import aSound from "../../assets/sounds/a.mp3";
+import dSound from "../../assets/sounds/d.mp3";
+import gSound from "../../assets/sounds/g.mp3";
+import bSound from "../../assets/sounds/b.mp3";
+import e2Sound from "../../assets/sounds/e2.mp3";
+
+const soundImports = {
+  e: new Audio(eSound),
+  a: new Audio(aSound),
+  d: new Audio(dSound),
+  g: new Audio(gSound),
+  b: new Audio(bSound),
+  e2: new Audio(e2Sound),
+};
+export default soundImports;
---
+++ src/global/constants.jsx
@@ -0,0 +1,12 @@
+const NOTES = ["e", "a", "d", "g", "b", "e2"];
+
+const KEY_TO_NOTE = {
+  a: "e",
+  s: "a",
+  d: "d",
+  f: "g",
+  g: "b",
+  h: "e2",
+};
+
+export { NOTES, KEY_TO_NOTE };
